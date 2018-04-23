71b6d030827b4601b2ac801067d7f9ed19fe253f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to find text using a keyboard shortcut
+
+
+This example shows how to execute the Find Next action by pressing the F3 shortcut after an end-user specified search options in the <strong>Find Text</strong> dialog. <br><br>To do this, handle the<strong> PdfViewer.KeyDown</strong> event. If the F3 key is pressed, call the overloaded <a href="https://documentation.devexpress.com/WindowsForms/DevExpress.XtraPdfViewer.PdfViewer.FindText.method(KsOC1Q)">PDFViewer.FindText </a> method and pass the text to search and search parameters obtained from the<strong> Find Text </strong>dialog to this method.  <br><br>The text search settings (e.g, search text, whole words, case sensitive) applied by an end-user in the <strong>Find Text</strong> dialog can be accessed using the<a href="https://documentation.devexpress.com/WindowsForms/DevExpress.XtraPdfViewer.PdfViewer.FindDialogOptions.property"> PdfViewer.FindDialogOptions</a> property.<br><br>
+
+<br/>
+
+
---
+++ VB/FindTextUsingShortcut/Form1.vb
@@ -0,0 +1,28 @@
+﻿Imports DevExpress.Pdf
+Imports DevExpress.XtraPdfViewer
+Imports System
+Imports System.Windows.Forms
+
+Namespace FindTextUsingShortcut
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+            pdfViewer1.LoadDocument("..\..\Report.pdf")
+        End Sub
+
+        Private Sub pdfViewer1_KeyDown(ByVal sender As Object, ByVal e As KeyEventArgs) Handles pdfViewer1.KeyDown
+            If e.KeyData = Keys.F3 Then
+                Dim parameters As New PdfTextSearchParameters()
+                Dim options As PdfFindDialogOptions = pdfViewer1.FindDialogOptions
+                parameters.CaseSensitive = options.CaseSensitive
+                parameters.WholeWords = options.WholeWords
+                pdfViewer1.FindText(options.Text, parameters)
+            End If
+        End Sub
+    End Class
+End Namespace
